078323fcc52fa2f4ee7bb4d5bb82a19ae3abe478
Julian Annen <julian.annen@web.de>
2019-04-16 21:30:21 +0200

    added Gene

--- src/Node.java
+++ src/Node.java
@@ -3,11 +3,19 @@ public class Node {
     private int x;
     private int y;
     private int value;
+    private int gene;
 
     public Node(int x, int y, int value) {
         this.x = x;
         this.y = y;
         this.value = value;
+    }
+
+    public Node(int x, int y, int value, int gene) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+        this.gene = gene;
     }
 
     public int getX() {
@@ -18,7 +26,20 @@ public class Node {
         return y;
     }
 
+    public void setXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     public int getValue() {
         return value;
+    }
+
+    public int getGene() {
+        return gene;
+    }
+
+    public void setGene(int gene) {
+        this.gene = gene;
     }
 }
