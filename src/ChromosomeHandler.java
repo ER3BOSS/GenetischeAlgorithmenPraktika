f7abb947575f73d50b12b09582d8de8a98f365d1
Julian Annen <julian.annen@web.de>
2019-04-16 21:29:07 +0200

    added basic ga support

---
+++ src/ChromosomeHandler.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public class ChromosomeHandler {
+
+    static private double mutationRate = 0.05;
+
+    public ChromosomeHandler() {
+    }
+
+    static Kette convertChromosome2NewGraph(ArrayList<Integer> chromosome, String sequence){
+        int x = 0;
+        int y = 0;
+        ArrayList<Node> kette2d = new ArrayList<>();
+        kette2d.add(new Node(0, 0, Character.getNumericValue(sequence.charAt(0))));
+
+        for (int i = 1; i < chromosome.size(); i++){
+            int gene = chromosome.get(i);
+            switch (gene) {
+                case 0: //up
+                    y += 1;
+                    kette2d.add(new Node(x,y,Character.getNumericValue(sequence.charAt(i))));
+                    break;
+                case 1: //down
+                    y -= 1;
+                    kette2d.add(new Node(x,y,Character.getNumericValue(sequence.charAt(i))));
+                    break;
+                case 2: //left
+                    x -= 1;
+                    kette2d.add(new Node(x,y,Character.getNumericValue(sequence.charAt(i))));
+                    break;
+                case 3: //right
+                    x += 1;
+                    kette2d.add(new Node(x,y,Character.getNumericValue(sequence.charAt(i))));
+                    break;
+            }
+        }
+
+        return new Kette(sequence, kette2d, chromosome);
+    }
+
+    static ArrayList<Integer> extractChromosome(ArrayList<Node> kette2d){
+        ArrayList<Integer> chromosome = new ArrayList<>();
+
+        for(Node node : kette2d){
+            chromosome.add(node.getGene());
+        }
+        return chromosome;
+    }
+
+    static void printChromosome (ArrayList<Integer> chromosome){
+        System.out.println();
+        System.out.println("### Chromosome ###");
+        System.out.println();
+        for (Integer gene : chromosome) {
+            System.out.print(gene);
+        }
+    }
+
+    static ArrayList<Integer> crossoverChromosome(ArrayList<Integer> chromosomeA, ArrayList<Integer> chromosomeB){
+        ArrayList<Integer> crossover = new ArrayList<>();
+
+        for (int i = 0; i < chromosomeA.size(); i++){
+            if (i < chromosomeA.size()/2){
+                crossover.add(chromosomeA.get(i));
+            }else{
+                crossover.add(chromosomeB.get(i));
+            }
+        }
+
+        return crossover;
+    }
+
+    static ArrayList<Integer> mutateChromosome(ArrayList<Integer> chromosome){
+        for (int i = 0; i < chromosome.size(); i++) {
+            double random = Math.random();
+            if (random <= mutationRate){
+                if (random < .50){
+                    int mutated = (chromosome.get(i) - 1) % 4;
+                    if (mutated == -1){ //for some reason -1 is possible with % in java
+                        mutated = 3;
+                    }
+                    chromosome.set(i, mutated);
+                }else {
+                    int mutated = (chromosome.get(i) + 2) % 4;
+                    chromosome.set(i,mutated);
+                }
+            }
+        }
+        return chromosome;
+    }
+}
---
+++ src/GenerationHandler.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class GenerationHandler {
+
+    private String sequence;
+    private ArrayList<Kette> individuals = new ArrayList<>();
+    private ImageCreator imageCreator = new ImageCreator();
+    private int maxGenerations = 0;
+    private int generationSize = 0;
+    private int newBloodAmount = 0;
+
+    public GenerationHandler(String sequence) {
+        this.sequence = sequence;
+    }
+
+
+    void initializeGeneration(int generationSize) {
+        for (int i = 0; i < generationSize; i++) {
+            individuals.add(new Kette(sequence));
+            individuals.get(i).generateByIntelligentRng();
+        }
+    }
+
+    void evolve(int maxGenerations, int generationSize, int newBloodAmount){
+        this.maxGenerations = maxGenerations;
+        this.generationSize = generationSize;
+        this.newBloodAmount = newBloodAmount;
+
+
+        for (int generation = 0; generation < maxGenerations; generation++){
+
+            //sort the list so the best individuals are on top (0 and 1)
+            individuals.sort((Kette ketteA, Kette ketteB) -> Double.compare(ketteB.calcFitness(),ketteA.calcFitness()));
+            individuals.subList(2, individuals.size()).clear(); // kill all but the 2 best
+
+            makeSomeBabys();
+
+            makeSomeMutants(generation);
+
+            makeSomeNewBlood(generation);
+
+            printAverageFitness(generation); //
+        }
+
+    }
+
+    private void makeSomeBabys(){ //Todo refactor!!!
+        //create 2 offspring's
+        ArrayList<Integer> chromosomeA = ChromosomeHandler.extractChromosome(individuals.get(0).getKette2d());
+        ArrayList<Integer> chromosomeB = ChromosomeHandler.extractChromosome(individuals.get(1).getKette2d());
+
+        ArrayList<Integer> childA = ChromosomeHandler.crossoverChromosome(chromosomeA,chromosomeB);
+        ArrayList<Integer> childB = ChromosomeHandler.crossoverChromosome(chromosomeB,chromosomeA);
+
+        individuals.add(ChromosomeHandler.convertChromosome2NewGraph(childA, sequence));
+        individuals.add(ChromosomeHandler.convertChromosome2NewGraph(childB, sequence));
+    }
+
+    private void makeSomeMutants(int generation){
+        int initialPop = individuals.size();
+        // fill the generationSize while leaving space for newBlood also no need to do that in the last gen
+        while (individuals.size() < generationSize - newBloodAmount && generation != maxGenerations -1 ){
+            int randomNum = ThreadLocalRandom.current().nextInt(0, initialPop);
+            ArrayList<Integer> chromosomeMutant = ChromosomeHandler.extractChromosome(individuals.get(randomNum).getKette2d());
+            ArrayList<Integer> mutant = ChromosomeHandler.mutateChromosome(chromosomeMutant);
+            individuals.add(ChromosomeHandler.convertChromosome2NewGraph(mutant, sequence));
+        }
+    }
+
+    private void makeSomeNewBlood(int generation){
+        while (individuals.size() < generationSize && generation != maxGenerations -1 ){
+            individuals.add(new Kette(sequence));
+            individuals.get(individuals.size()-1).generateByIntelligentRng();
+        }
+    }
+
+    //todo write a actually good log -> as .txt
+    private void printAverageFitness(int generation){
+        double avr = 0;
+        for (Kette kette : individuals){
+            avr += kette.calcFitness();
+        }
+        avr = avr/individuals.size();
+        System.out.println( generation + ". Average: " + avr);
+    }
+
+    void printResult() { //todo: move image creation somewhere else
+        for (int i = 0; i < individuals.size(); i++){
+            imageCreator.createImage(individuals.get(i).getKette2d(), Integer.toString(i)+ ".png");
+            System.out.println();
+            individuals.get(i).printValues();
+        }
+    }
+
+}
