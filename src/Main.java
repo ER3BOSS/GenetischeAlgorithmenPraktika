59754805618078ecfb08390b929d7c154400662c
Julian Annen <julian.annen@web.de>
2019-05-09 11:56:37 +0200

    added Test Strings

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.io.IOException;
 
-        /* Test sequences
+
+
+
+
+class Main {
+
+
+
+
+    public static void main(String[] args) throws IOException {
+
         String SEQ20 = "10100110100101100101";
         String SEQ24 = "110010010010010010010011";
         String SEQ25 = "0010011000011000011000011";
         String SEQ36 = "000110011000001111111001100001100100";
         String SEQ48 = "001001100110000011111111110000001100110010011111";
         String SEQ50 = "11010101011110100010001000010001000101111010101011";
-        */
 
-class Main {
-    public static void main(String[] args) throws IOException {
-
-        GenerationHandler generationHandler = new GenerationHandler("11010101011110100010001000010001000101111010101011");
+        GenerationHandler generationHandler = new GenerationHandler(SEQ50);
         generationHandler.initializeGeneration(1000);
 
         generationHandler.evolve(
@@ -20,7 +26,7 @@ class Main {
                 0,
                 0.1,
                 0.1,
-                SelectType.TOURNAMENT
+                SelectType.FITNESS
         );
 
         generationHandler.drawResult(1);
